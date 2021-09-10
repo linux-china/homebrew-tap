52b02dedf988679861f231ffba5c122883728e58
linux_china <libing.chen@gmail.com>
2021-09-09 18:30:03 -0700

    rename

--- maven-toolchains-cli.rb
+++ maven-toolchains-cli.rb
@@ -1,5 +1,5 @@
 class MavenToolchainsCli < Formula
-  desc "Maven Toolchains CLI"
+  desc "CLI for Apache Maven toolchains.xml"
   homepage "https://github.com/linux-china/maven-toolchains-cli"
   url "https://github.com/linux-china/maven-toolchains-cli/releases/download/0.1.0/mt-0.1.0-mac-x64.tar"
   sha256 "ac90a384e8fbc9c455ce6ebdd3e034e7a0c272b67fcb0a4ed0c7df95d92b35e9"
@@ -8,5 +8,9 @@ class MavenToolchainsCli < Formula
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/mt"
+  end
+
+  test do
+    system "#{bin}/program/mt"
   end
 end
