ae766b6d1c6e37e0bff4accdf70257ff992956bc
linux_china <libing.chen@gmail.com>
2021-09-09 18:21:36 -0700

    Polishing

--- mt.rb
+++ mt.rb
@@ -1,9 +1,9 @@
-class MavenToolChainsCLI < Formula
-  desc "CLI for Maven toolchains.xml"
-  homepage 'https://github.com/linux-china/maven-toolchains-cli'
-  version '0.1.0'
-  url "https://github.com/linux-china/maven-toolchains-cli/releases/download/#{version}/mt-#{version}-mac-x64.tar"
-  sha256 'ac90a384e8fbc9c455ce6ebdd3e034e7a0c272b67fcb0a4ed0c7df95d92b35e9'
+class MavenToolchainsCli < Formula
+  desc "Maven Toolchains CLI"
+  homepage "https://github.com/linux-china/maven-toolchains-cli"
+  url "https://github.com/linux-china/maven-toolchains-cli/releases/download/0.1.0/mt-0.1.0-mac-x64.tar"
+  sha256 "ac90a384e8fbc9c455ce6ebdd3e034e7a0c272b67fcb0a4ed0c7df95d92b35e9"
+  license "Apache-2.0"
 
   def install
     libexec.install Dir["*"]
