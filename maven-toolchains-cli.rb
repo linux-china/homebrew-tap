15c2f4a3e7b3e51fefaba53558db6018734e9392
Libing Chen <libing.chen@gmail.com>
2021-09-17 19:08:22 -0700

    Update to version 0.2.0

--- maven-toolchains-cli.rb
+++ maven-toolchains-cli.rb
@@ -1,9 +1,9 @@
 class MavenToolchainsCli < Formula
   desc "CLI for Apache Maven toolchains.xml"
   homepage "https://github.com/linux-china/maven-toolchains-cli"
-  version "0.1.0"
-  url "https://github.com/linux-china/maven-toolchains-cli/releases/download/0.1.0/mt-0.1.0-mac-x64.tar"
-  sha256 "ac90a384e8fbc9c455ce6ebdd3e034e7a0c272b67fcb0a4ed0c7df95d92b35e9"
+  version "0.2.0"
+  url "https://github.com/linux-china/maven-toolchains-cli/releases/download/0.2.0/mt-0.2.0-mac-x64.tar"
+  sha256 "1b4b250a5a82eee4e30223fea449be1b74d1431dca08d7a72b673ed702df8178"
   license "Apache-2.0"
 
   def install
