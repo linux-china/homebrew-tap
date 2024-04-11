a32bf7688e0fc71e93c59bb2a5be8fc331b60c81
Libing Chen <libing.chen@gmail.com>
2024-04-11 22:52:22 +0800

    update to version 0.5.4

--- zawk.rb
+++ zawk.rb
@@ -5,12 +5,12 @@ class Zawk < Formula
     license 'Apache-2.0'
 
     if OS.mac? && Hardware::CPU.intel?
-      url 'https://github.com/linux-china/zawk/releases/download/v0.5.3/zawk-0.5.3-x86_64-apple-darwin.tar'
-      sha256 'aeaa86a8711faf5aba9e7c5c81464dac6f73ab28d1d5f9730932ea3047454e03'
+      url 'https://github.com/linux-china/zawk/releases/download/v0.5.4/zawk-0.5.4-x86_64-apple-darwin.tar'
+      sha256 '4a4b37cfc36676ca7fc13bfafb702a215041265e2e50b53457d5bff176a0e091'
     end
     if OS.mac? && Hardware::CPU.arm?
-      url 'https://github.com/linux-china/zawk/releases/download/v0.5.3/zawk-0.5.3-aarch64-apple-darwin.tar'
-      sha256 'ed1d78d6d1e2c950a8c140d8f22ed1066893f88f3da684b844fea13334f4fa51'
+      url 'https://github.com/linux-china/zawk/releases/download/v0.5.4/zawk-0.5.4-aarch64-apple-darwin.tar'
+      sha256 '27cfcdcf0062505e0f92574dd15aa1fcc07151cb2a6d52cfbd4360b6889f8f51'
     end
 
     def install
