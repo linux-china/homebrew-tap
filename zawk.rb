ddff8711a24a0e71c41fcd41a8a6f003185576b1
Libing Chen <libing.chen@gmail.com>
2024-07-09 23:11:25 +0800

    chore: update zawk to version 0.5.9

--- zawk.rb
+++ zawk.rb
@@ -1,16 +1,16 @@
 class Zawk < Formula
     desc 'zawk - An efficient awk-like language by Rust with stdlib support'
     homepage 'https://github.com/linux-china/zawk'
-    version '0.5.8'
+    version '0.5.9'
     license 'Apache-2.0'
 
     if OS.mac? && Hardware::CPU.intel?
-      url 'https://github.com/linux-china/zawk/releases/download/v0.5.8/zawk-0.5.8-x86_64-apple-darwin.tar'
-      sha256 '578e53a1ca4cd9bdd1359136bfb1de116c04decff92c7b20ead6d8bb94b22003'
+      url 'https://github.com/linux-china/zawk/releases/download/v0.5.9/zawk-0.5.9-x86_64-apple-darwin.tar'
+      sha256 '5a318425b5377ce0172a0085132006a911248740a6b0e81a5eb37d83baf6366d'
     end
     if OS.mac? && Hardware::CPU.arm?
-      url 'https://github.com/linux-china/zawk/releases/download/v0.5.8/zawk-0.5.8-aarch64-apple-darwin.tar'
-      sha256 '4b860863fae70a89d64e9baf27ed351df19196cff2506a48de9af69095e4b207'
+      url 'https://github.com/linux-china/zawk/releases/download/v0.5.9/zawk-0.5.9-aarch64-apple-darwin.tar'
+      sha256 '561d197946c2b6b8152e324ae81de8ec739464ad308dc9476603dba8c819c43e'
     end
 
     def install
