5f1385a651512251aeb85e263ba14c0ae0bd9064
Libing Chen <libing.chen@gmail.com>
2025-09-11 22:21:05 +0800

    build: update to dotenvx 0.4.18

--- Formula/dotenvx-rs.rb
+++ Formula/dotenvx-rs.rb
@@ -1,25 +1,25 @@
 class DotenvxRs < Formula
   desc "Dotenvx is a Rust command-line/library to encrypt your .env files - limiting their attack vector while retaining their benefits"
   homepage "https://github.com/linux-china/dotenvx-rs"
-  version "0.4.17"
+  version "0.4.18"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/linux-china/dotenvx-rs/releases/download/v0.4.17/dotenvx-rs-aarch64-apple-darwin.tar.xz"
-      sha256 "9541633c919443e8f4c3ded02d0b3b55c2babafcf63390c5d790e6725e56daea"
+      url "https://github.com/linux-china/dotenvx-rs/releases/download/v0.4.18/dotenvx-rs-aarch64-apple-darwin.tar.xz"
+      sha256 "2c5ab8d799f9e85535a42cf4a38ea92d86fd85177dbe2c98493a5bb9de9ca2de"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/linux-china/dotenvx-rs/releases/download/v0.4.17/dotenvx-rs-x86_64-apple-darwin.tar.xz"
-      sha256 "74cd26d0ebf7297184622ddb2598992bad84365fa4b7036859f0372998be9751"
+      url "https://github.com/linux-china/dotenvx-rs/releases/download/v0.4.18/dotenvx-rs-x86_64-apple-darwin.tar.xz"
+      sha256 "f97fa4f4cdcc10414f61feef2733c7affb916e02c87a26278b52a0e92799b523"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/linux-china/dotenvx-rs/releases/download/v0.4.17/dotenvx-rs-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "16d40772d25327a10db86cee8eef4a9b8044d3236512ed61699fc825f993a700"
+      url "https://github.com/linux-china/dotenvx-rs/releases/download/v0.4.18/dotenvx-rs-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "e22d237eec7dc84c1086514549561607dbea1dca64cea0cf1971248a745168d1"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/linux-china/dotenvx-rs/releases/download/v0.4.17/dotenvx-rs-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "89c61ce3352604956c9cef4a2d1db5963635c63a8bc1678b9d2d7277fa5d048e"
+      url "https://github.com/linux-china/dotenvx-rs/releases/download/v0.4.18/dotenvx-rs-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "a1e9929f01707bb295d555d05c563c6263dd680fff6b507fd42d60abe6190e80"
     end
   end
   license "MIT"
